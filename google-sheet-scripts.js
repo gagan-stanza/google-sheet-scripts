6cd5508cacd16570561f871312d2c7c9a525b2f0
gagan-stanza <gagan.rajput@stanzaliving.com>
2026-05-27 00:12:31 +0530

    testing

--- google-sheet-scripts.js
+++ google-sheet-scripts.js
@@ -1,5 +1,12 @@
-function TEST() {
-  var sheet = SpreadsheetApp.openById("1KHXqh8eXYKF4dWPY51Z7p7N1X60MaHkTNHKmT2TIgRo").getSheetByName("Test1818");
-  sheet.getRange("A1").setValue("Hello World!");
-  sheet.getRange("B1").setValue("Setup is working!");
+// ===== GOOGLE SHEET IDs =====
+var SHEET_A = "1KHXqh8eXYKF4dWPY51Z7p7N1X60MaHkTNHKmT2TIgRo"; // Sheet: "All_Information"
+
+// ===== TAB NAMES =====
+var SHEET_A_TAB_A = "Test1818"; // Sheet: "All_Information" >> Tab: "Test1818"
+
+// ===== FUNCTIONS =====
+function FORMAT() {
+  var sheet = SpreadsheetApp.openById(SHEET_A).getSheetByName(SHEET_A_TAB_A);
+  sheet.getRange("C1").setValue("This is cell: C1");
+  sheet.getRange("D1").setValue("This is cell: D1");
 }
