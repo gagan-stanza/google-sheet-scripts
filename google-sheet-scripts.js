0c92fc18c018a1a967d4090a98d9fe7cd76124fb
gagan-stanza <gagan.rajput@stanzaliving.com>
2026-05-28 15:47:54 +0530

    mirror live booking dump

--- google-sheet-scripts.js
+++ google-sheet-scripts.js
@@ -2,11 +2,21 @@
 var SHEET_A = "1KHXqh8eXYKF4dWPY51Z7p7N1X60MaHkTNHKmT2TIgRo"; // Sheet: "All_Information"
 
 // ===== TAB NAMES =====
-var SHEET_A_TAB_A = "Test1818"; // Sheet: "All_Information" >> Tab: "Test1818"
+var SHEET_A_TAB_A = "LiveBooking_Import"; // Sheet: "All_Information" >> Tab: "LiveBooking_Import"
+var SHEET_A_TAB_B = "LiveBooking_Import_Mirror"; // Sheet: "All_Information" >> Tab: "LiveBooking_Import_Mirror"
 
 // ===== FUNCTIONS =====
-function FORMAT() {
-  var sheet = SpreadsheetApp.openById(SHEET_A).getSheetByName(SHEET_A_TAB_A);
-  sheet.getRange("C1").setValue("This is cell: C1");
-  sheet.getRange("D1").setValue("This is cell: D1");
+function MIRROR() {
+  var ss = SpreadsheetApp.openById(SHEET_A);
+  var source = ss.getSheetByName(SHEET_A_TAB_A);
+  var destination = ss.getSheetByName(SHEET_A_TAB_B);
+
+  // Get all data from source tab
+  var data = source.getDataRange().getValues();
+
+  // Clear destination tab first
+  destination.clearContents();
+
+  // Copy data to destination tab
+  destination.getRange(1, 1, data.length, data[0].length).setValues(data);
 }
